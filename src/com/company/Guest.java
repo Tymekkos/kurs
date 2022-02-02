ddbb50addf2f7503bfd7e3f508631525fff3b593
Tymekkos <tymek.koson@gmail.com>
2022-02-02 16:58:34 +0100

    02.02

--- src/com/company/Guest.java
+++ src/com/company/Guest.java
@@ -1,4 +1,39 @@
 package com.company;
 
 public class Guest {
+    private String name;
+    private String meal;
+    private int phoneNumber;
+    private boolean isVegan;
+
+    public Guest(String name, String meal, int phoneNumber, boolean isVegan) {
+        this.name = name;
+        this.meal = meal;
+        this.phoneNumber = phoneNumber;
+        this.isVegan = isVegan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public boolean isVegan() {
+        return isVegan;
+    }
+
+    public void guestInformation(){
+        System.out.println("imie: " + name);
+        System.out.println("ulubiony posilek: " + meal);
+        System.out.println("numer tel: " + phoneNumber);
+        String vegan = isVegan ? "tak" : "nie";
+        System.out.println("weganin?" + vegan);
+    }
 }
--- src/com/company/PartyList.java
+++ src/com/company/PartyList.java
@@ -5,18 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PartyList {
-    private List<String> guests = new ArrayList<>();
+    private List<Guest> guests = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
     public void addGuest() {
         System.out.println("podaj imie goscia");
         String name = scanner.nextLine();
-        guests.add(name);
+
+        System.out.println("podaj preferowany posilek");
+        String meal = scanner.nextLine();
+
+        System.out.println("podaj numer telefonu");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("czy weganin (y/n)");
+        String veganString = scanner.nextLine();
+
+        boolean vegan;
+
+        if(veganString.equals("y") || veganString.equals("Y")){
+            vegan = true;
+        }else{
+            vegan = false;
+        }
+
+        Guest guest = new Guest(name, meal, number, vegan);
+        guests.add(guest);
     }
 
     public void displayGuests(){
-        for(String guest : guests){
-            System.out.println(guest);
+        for(Guest guest : guests){
+            guest.guestInformation();
         }
     }
 }
